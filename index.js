ef6f572e6703d665472206d2f932251f4da3d9f6
Gleb <3696732@mail.ru>
2019-10-27 21:45:00 +0300

    feat: add img for controls

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 import './style.scss';
+
+import matrix4x4 from './json/4x4';
+import matrix32x32 from './json/32x32';
 document.addEventListener("DOMContentLoaded", canvas);
+
 function canvas() {
-    const cvs = document.getElementById('canvas');
+    const cvs = document.getElementById('canvas'),
+          scale = window.innerWidth/9.714,
+          height = matrix4x4[0].length,
+          width = matrix4x4.length;
     let context = cvs.getContext('2d');
 
-    // cvs.width = 1000;
-    // cvs.height = 1000;
 
-    // window.addEventListener("resize", function () {
-    //     w = cvs.width = window.innerWidth;
-    //     h = cvs.height = window.innerHeight;
-    // });
+    window.addEventListener("resize", function () {
+        // if (window.innerWidth > 1400 && window.innerHeight > 900) {
+        //     cvs.width = 724;
+        //     cvs.height = 724;
+        // }
 
-    context.fillStyle='red';
-    context.fillRect(0, 0, cvs.width, cvs.height);
+        // if (window.innerWidth  1400 && window.innerHeight > 900) {
+        //     cvs.width = 724;
+        //     cvs.height = 724;
+        // }
+        // cvs.width = window.innerWidth;
+        // cvs.height = window.innerHeight;
+    });
+
+    for(let row = 0; row < height; row++) {
+        for(let col = 0; col < width; col++) {
+            context.fillStyle = `#${matrix4x4[row][col]}`; // Set the color to the one specified
+            context.fillRect(row * scale, col * scale, scale, scale); // Actually draw the rectangle
+        }
+    }
+
 }
