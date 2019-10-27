844fecc6f4241ef61e64b0f21e2c83b13efe95c8
Gleb <3696732@mail.ru>
2019-10-27 20:19:59 +0300

    feat: add element to site

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-
 import './style.scss';
-let a = 'gdgd';
-console.log(a);
+document.addEventListener("DOMContentLoaded", canvas);
+function canvas() {
+    const cvs = document.getElementById('canvas');
+    let context = cvs.getContext('2d');
+
+    // cvs.width = 1000;
+    // cvs.height = 1000;
+
+    // window.addEventListener("resize", function () {
+    //     w = cvs.width = window.innerWidth;
+    //     h = cvs.height = window.innerHeight;
+    // });
+
+    context.fillStyle='red';
+    context.fillRect(0, 0, cvs.width, cvs.height);
+}
